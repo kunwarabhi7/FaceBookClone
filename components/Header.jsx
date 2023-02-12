21d53e58d76d479e69235edb9afa452725371f57
Abhishek <abhishekparmar162@gmail.com>
2023-02-12 14:14:36 +0530

    setup right header

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,13 @@ import {BsFacebook,BsSearch} from 'react-icons/bs'
 import {AiFillHome} from 'react-icons/ai'
 import {MdOndemandVideo,MdOutlineGroups} from 'react-icons/md'
 import {CiShop} from 'react-icons/ci'
-import {RiGamepadLine} from 'react-icons/ri'
+import {RiGamepadLine,RiNotification2Fill} from 'react-icons/ri'
+import {CgMenuGridR} from 'react-icons/cg'
+import {TbBrandMessenger} from 'react-icons/tb'
+import {RxAvatar} from 'react-icons/rx'
+
+
+
 const Header = () => {
   return (
     <div className='flex justify-between p-4  '>
@@ -19,7 +25,7 @@ const Header = () => {
                 </form>
              </div>
              {/* center */}
-      <div className=' flex justify-evenly'>
+      <div className=' flex justify-evenly w-[700px]'>
         <AiFillHome size={30} className='text-blue-500'/>
         <MdOndemandVideo size={30} className='text-blue-500'/>
         <CiShop size={30} className='text-blue-500'/>
@@ -27,7 +33,12 @@ const Header = () => {
         <RiGamepadLine size={30} className='text-blue-500'/>
             </div>
       {/* right */}
-      <div>Right</div>
+      <div className='w-44 flex justify-evenly'>
+        <CgMenuGridR size={40} className='text-white hover:text-blue-500 cursor-pointer rounded-full p-1'/>
+        <TbBrandMessenger size={40} className='text-white hover:text-blue-500 cursor-pointer rounded-full p-1'/>
+        <RiNotification2Fill size={40} className='text-white p-1 bg-gray-500 hover:text-blue-500 rounded-full cursor-pointer'/>
+        <RxAvatar size={40} className='text-white hover:text-blue-500 bg-gray-500 cursor-pointer rounded-full p-1' />
+        </div>
     </div>
   )
 }
