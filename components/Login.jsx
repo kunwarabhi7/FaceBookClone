02331be7387a01c3ccfa67923212c51ee144d3f7
Abhishek <abhishekparmar162@gmail.com>
2023-02-12 18:47:04 +0530

    style fb button

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import {FaFacebook} from 'react-icons/fa' 
 const Login = () => {
   return (
-    <div>
-      Login From Facebook
+    <div className='grid place-items-center py-16'>
+      <FaFacebook size={300} className='text-blue-500'/>
+      <button className='bg-blue-500 text-white text-xl font-bold my-16 text-center rounded-full p-12'>Login With Facebook</button>
     </div>
   )
 }
