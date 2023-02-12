7663473d452b7f6d4acdf6baebff75975ba46449
Abhishek <abhishekparmar162@gmail.com>
2023-02-12 13:36:25 +0530

    fix css

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,15 +2,15 @@ import {BsFacebook,BsSearch} from 'react-icons/bs'
 
 const Header = () => {
   return (
-    <div className='flex justify-between p-4'>
+    <div className='flex justify-between p-4  '>
         {/* left */}
              <div className='flex'>
-                <BsFacebook className='text-blue-500' size={30}/>
+                <BsFacebook className='text-blue-500   mt-1' size={30}/>
                 <form>
-<div className='flex'>
+<div className='flex bg-[rgb(58,59,60)] h-10 md:w-60 rounded-full p-3 ml-3 mb-2 w-10' >
 
 <BsSearch />
-<input type="text" placeholder='Search Facebook' />
+<input type="text" placeholder='Search Facebook' className='bg-[rgb(58,59,60)] ml-4 hidden ' />
 </div>
 
                 </form>
