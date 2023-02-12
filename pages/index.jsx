2f350da8ea08080bfd1946eeecf25ba7c09f4a31
Abhishek <abhishekparmar162@gmail.com>
2023-02-12 12:02:12 +0530

    install react-icons

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <div>FaceBook</div>
+    <div className='text-blue-500 bg-gray-500'>FaceBook</div>
     </>
   )
 }
