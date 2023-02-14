877672374f68fef1a16b956d3fac001152da1994
Abhishek <abhishekparmar162@gmail.com>
2023-02-14 15:07:25 +0530

    get image in sidebar

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useSession } from 'next-auth/react'
+import {AiFillHome} from 'react-icons/ai'
+import Image from 'next/image'
 
 const SideBar = () => {
+  const {data:session,loading}  = useSession()
   return (
     <div>
-   Sidebar   
+   <div>
+
+   <AiFillHome />
+   <h1>Home</h1>
+   </div>
+   <div>
+    <Image height={40} width={40} src={session?.user.image} />
+<h2>{session?.user.name}</h2>
+   </div>
+
     </div>
   )
 }
