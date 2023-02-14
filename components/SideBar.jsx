d8622e8347151b89e0e9c025d8b34723f208aca5
Abhishek <abhishekparmar162@gmail.com>
2023-02-14 17:10:30 +0530

    sidebar styling

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -3,6 +3,7 @@ import { useSession } from 'next-auth/react'
 import {AiFillHome} from 'react-icons/ai'
 import {MdOutlineOndemandVideo} from 'react-icons/md'
 import {FaUserFriends} from 'react-icons/fa'
+import {CgMenuGridO} from 'react-icons/cg'
 import {SiHomeassistantcommunitystore , SiFacebookgaming} from 'react-icons/si'
 import SideBarComponents from './SideBarComponents'
 
@@ -13,13 +14,17 @@ const SideBar = () => {
 
 <SideBarComponents icon={<AiFillHome size={20}/>} title='Home' />
 <SideBarComponents image={session.user.image}  name={session.user.name}/>  
-<hr className='border-1 border-gray-100 mt-4 w-12 md:w-36' />
+<hr className='border-1 border-gray-700 mt-4 w-12 md:w-36' />
 <SideBarComponents  icon={<MdOutlineOndemandVideo size={20} />} title='Watch' />
 <SideBarComponents icon={<FaUserFriends size={20}/>} title='Friends' />
 <SideBarComponents icon={<SiHomeassistantcommunitystore />} title='MarketPlace' />
 <SideBarComponents icon={<SiFacebookgaming />} title='Gaming' />
+<h1 className='flex w-72 h-8 p-2'>
+<CgMenuGridO className='w-8 h-8 text-gray-500 ' size={20} />
+<h2 className='h-5 hidden md:flex ml-2 mt-1 '>See All</h2>
+</h1>
+<hr className='border-1 border-gray-700 mt-4 w-12 md:w-36' />
 
- 
  
    </div>
   )
