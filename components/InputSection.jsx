e5e9ce0f0779847f0a6bbe1cab42f3c7f513ab68
Abhishek <abhishekparmar162@gmail.com>
2023-02-15 15:53:13 +0530

    added all icons

--- components/InputSection.jsx
+++ components/InputSection.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useSession } from 'next-auth/react'
 import Image from 'next/image'
-
+import {TiVideo} from 'react-icons/ti'
+import {IoImages} from 'react-icons/io5'
+import {GoSmiley} from 'react-icons/go'
 
 const InputSection = () => {
     const {data:session} = useSession()
@@ -19,9 +21,25 @@ e.preventDefault()
 <input placeholder= {`What's on your mind , ${session.user.name} `} className='p-2 w-[500px] h-10 rounded-full ml-3' />
 <button onClick={SubmitPost} className='hidden'>Submit</button>
 </form>
-<div className='h-10 w-[558px]'>
-</div>
         </div>
+        <hr className='my-4 w-[550px]' />
+<div className='h-10 w-[558px] flex justify-evenly'>
+    <div className='flex items-center '>
+
+    <TiVideo size={25} className='text-red-500 mr-2'/>
+    <h1 >Live Video</h1>
+    </div>
+    <div className='flex items-center'>
+
+    <IoImages size={25} className='text-green-500' />
+    <h1>Photo/video</h1>
+    </div>
+    <div className='flex items-center'>
+
+    <GoSmiley size={25} className='text-yellow-400' />
+    <h1>Feeling/activity</h1>
+    </div>
+</div>
       
     </div>
   )
