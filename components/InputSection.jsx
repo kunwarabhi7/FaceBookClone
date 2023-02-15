003af2f755477011e484d34190c98961eec1187d
Abhishek <abhishekparmar162@gmail.com>
2023-02-15 15:32:54 +0530

    added input section

--- components/InputSection.jsx
+++ components/InputSection.jsx
@@ -5,11 +5,22 @@ import Image from 'next/image'
 
 const InputSection = () => {
     const {data:session} = useSession()
-  return (
-    <div>
-        <div>
-        <Image src={session?.user.image} height={20} width={38} className='text-white hover:text-blue-500 bg-gray-500 cursor-pointer rounded-full  ml-1' />
 
+    const SubmitPost=(e) =>{
+e.preventDefault()
+    }
+
+  return (
+    <div className='w-96 h-25 mt-8 ml-4 shadow-md '>
+        <div className='flex'>
+        <Image src={session?.user.image} height={20} width={38} className='text-white hover:text-blue-500 bg-gray-500 cursor-pointer rounded-full  ml-1' />
+<form>
+
+<input placeholder= {`What's on your mind , ${session.user.name} `} className='p-2 w-[500px] h-10 rounded-full ml-3' />
+<button onClick={SubmitPost} className='hidden'>Submit</button>
+</form>
+<div className='h-10 w-[558px]'>
+</div>
         </div>
       
     </div>
