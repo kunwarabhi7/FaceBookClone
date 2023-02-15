dd7b9c7ca637d4821b7126f2829bab324501fd6a
Abhishek <abhishekparmar162@gmail.com>
2023-02-15 14:06:17 +0530

    added comments

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,6 +4,7 @@ import {TfiInstagram} from 'react-icons/tfi'
 const Feed = () => {
   return (
     <div className='w-[590px] bg-[rgb(36,37,38)] ml-32 mt-6'>
+      {/* Top Section */}
       <div className='flex h-18 justify-evenly p-8'>
         <div className='flex w-70  border-b-2 border-blue-500'>
         <MdOutlineAutoStories size={40} className='text-blue-500 w-36' />
@@ -14,8 +15,11 @@ const Feed = () => {
         <h1>Reels</h1>
         </div>
       </div>
+      {/* Stories */}
       <div>Story</div>
+      {/* Input section */}
       <div>Input section</div>
+      {/* Main Section */}
       <div>Feed Section</div>
     </div>
   )
